768c7bfbfee3838a7a0ad3d53242449021e0b6eb
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-08-19 12:49:06 +0500

    change api in frontend:

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -96,7 +96,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-40",
+  baseUrl: "http://api.mesto.antonovskaya.nomoredomains.sbs",
   headers: {
     authorization: "242a0c65-83ea-4aa5-aad5-7b82cd182540",
     "Content-Type": "application/json",
